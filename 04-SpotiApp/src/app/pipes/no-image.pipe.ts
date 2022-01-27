0ef275df45b338ebcfce9fcd94f081f88cf79d9c
Jorge Perez <jpfernandez098@gmail.com>
2022-01-27 09:18:53 +0100

    pipe funciona

--- 04-SpotiApp/src/app/pipes/no-image.pipe.ts
+++ 04-SpotiApp/src/app/pipes/no-image.pipe.ts
@@ -5,8 +5,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class NoImagePipe implements PipeTransform {
 
-  transform(value: any): any  {
-    console.log("SFSDGFDDFGHJFGJK");
+  transform(value: any): any   {
+   if (value.length == 0){
+     return "assets/img/noimage.png"
+   }else{
+     return value[1].url;
+   }
 
 
   }
